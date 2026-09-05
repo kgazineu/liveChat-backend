05fdaf50ed86d10feadc6b57d52944ef3d11138e
Kaian <kaiangazineu@gmail.com>
2026-09-05 20:42:43 -0300

    test - ci

--- src/test/java/com/example/liveChat/SecurityIntegrationTests.java
+++ src/test/java/com/example/liveChat/SecurityIntegrationTests.java
@@ -152,6 +152,18 @@ class SecurityIntegrationTests {
         mvc.perform(get("/actuator/env")).andExpect(status().isUnauthorized());
     }
 
+    @Test
+    void apiDocumentationIsPublic() throws Exception {
+        mvc.perform(get("/v3/api-docs"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.info.title").value("LiveChat API"));
+        mvc.perform(get("/swagger-ui.html"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/swagger-ui/index.html"));
+        mvc.perform(get("/swagger-ui/index.html"))
+                .andExpect(status().isOk());
+    }
+
     private User user() {
         return users.save(new User("Test", UUID.randomUUID() + "@example.test", passwordEncoder.encode("password")));
     }
